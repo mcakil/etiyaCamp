f43aac7cd0eb47922431e1cda00d1c35750c6120
mcakil <49635846+mcakil@users.noreply.github.com>
2022-07-21 14:57:30 +0300

    Create Ders25.java

---
+++ ikinciGunOrnekleri/Ders25.java
@@ -0,0 +1,29 @@
+public class Ders25 {
+	public static void main(String[] args) {
+		String mesaj = "Bugün hava çok güzel.";
+		String yeniMesaj = mesaj.substring(0, 2);
+		System.out.println(yeniMesaj);
+		int sayi = topla(15, 7);
+		System.out.println(sayi);
+	}
+	
+	public static void ekle() {
+		System.out.println("Eklendi");
+	}
+	
+	public static void sil() {
+		System.out.println("Silindi");
+	}
+
+	public static void guncelle() {
+		System.out.println("Güncellendi");
+	}
+	
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1+sayi2;
+	}
+	
+	public static String sehirVer() {
+		return "Ankara";
+	}
+}
